55e08173a8ee05e520f3dd087309c30ca2a5f912
Anais-Z <anaiszed447@gmail.com>
2026-09-11 23:33:13 -0400

    Created divs to add text.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     
+     <div>Welcome to ContextLM</div>
+     <div>Please accept Terms of Service before using</div>
+     <div>ContextLM is a web application inspired by NotebookLM, which allows users to upload 
+      research papers and ask the app any questions for context regarding their paper. This app is not a
+      replacepemtn or to be compete with NotebookLM but for learning purposes. Even though no data will be saved,
+      please do not upload any document with your personal information or anyone's on it. Also please do not upload
+      any document containing any innapropriate content. All documents to be uploaded should be an approved research paper
+      published by credtable sites such as IEEE. 
+
+      When chatting with the chatbot, only ask questions regarding the research paper uploaded as it is programmed to read the paper.
+      Do not ask any personal questions or anything else unrelated to the specific paper. Do not use any vulgar or extreme language.
+      Upon leaving this website, any information provided to the chatbot will not be saved and be erased.
+
+      If you accept these tersms and serves please click the Agree buttton
+     </div>
     </>
   )
 }
